9c1fed2125d410c4616c7655192855f8e04bfe56
yian <yianzhou2@outlook.com>
2018-07-16 00:21:00 +0800

    LeetCode 1

---
+++ src/algorithm/LeetCode1.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+// https://leetcode.com/problems/two-sum/description/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int length = nums.length;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i=0;i<length;i++) {
+            map.put(nums[i], i);
+        }
+        for (int i=0;i<length;i++) {
+            int diff = target-nums[i];
+            if (map.containsKey(diff) && map.get(diff)!=i ) {
+                result[0] = i;
+                result[1] = map.get(diff);
+                return result;
+            }
+        }
+        return result;
+    }
+}
