4246919261e088519659783515a2e3b76ea2cc2e
yian <yianzhou2@outlook.com>
2018-08-15 18:08:32 +0800

    New solution

---
+++ src/algorithm/LeetCode22.java
@@ -0,0 +1,30 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/generate-parentheses/description/
+
+public class LeetCode22 {
+    private List<String> result = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        dfs("", 0, 0, n);
+        return result;
+    }
+
+    private void dfs(String combination, int left, int right, int n) {
+        if (left == n && right == n) {
+            result.add(combination);
+            return;
+        }
+        if (left < n) {
+            dfs(combination+"(", left+1, right, n);
+        }
+        if (left > right) {
+            dfs(combination+")", left, right+1, n);
+        }
+    }
+
+    // output: ["((()))","(()())","(())()","()(())","()()()"]
+}
