577536e51e4b926c6a34cd6f5c24a8a53e4449c5
yian <yianzhou2@outlook.com>
2018-08-18 14:35:40 +0800

    Update solution 2

--- src/algorithm/LeetCode2.java
+++ src/algorithm/LeetCode2.java
@@ -5,33 +5,33 @@ import com.leetcode.ListNode;
 // https://leetcode.com/problems/add-two-numbers/description/
 
 public class LeetCode2 {
-    // Solution version 1
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
 
         ListNode result = new ListNode(0);
-        ListNode firstNode = result;
+        ListNode firstNode = result; // The first node is no use
         int carry = 0;
 
-        while (l1 != null || l2 != null) {
-            int val1 = l1 == null ? 0 : l1.val;
-            int val2 = l2 == null ? 0 : l2.val;
-            int sum = val1 + val2 + carry;
-            ListNode newNode = new ListNode(sum % 10);
-            carry = sum / 10;
-            result.next = newNode;
-            result = result.next;
+        while (l1 != null || l2 != null) { // stop when l1 and l2 both reach the end
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
 
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+            result.next = new ListNode(carry % 10);
+            result = result.next;
+            carry /= 10;
         }
 
-        if (carry > 0) {
+        if (carry > 0) { // the remain carry should take a new node
             result.next = new ListNode(carry);
         }
 
         return firstNode.next; // The first node is no use
     }
-
 }
