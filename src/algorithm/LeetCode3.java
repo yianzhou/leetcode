929a28a6d5525d04d0e3a763effff8d2bc528511
yian <yianzhou2@outlook.com>
2018-08-21 10:31:19 +0800

    Update

--- src/algorithm/LeetCode3.java
+++ src/algorithm/LeetCode3.java
@@ -1,20 +1,18 @@
 package algorithm;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class LeetCode3 {
 
-    /* keep a HashMap (Character, position)
-	 * keep two pointers which define the max substring - [left, right]
-	 * move the right pointer to scan through the string, and meanwhile update the HashMap.
+    /* Keep a HashMap (Character, position).
+	 * keep two pointers which define the max substring - [left, right].
+	 * Move the right pointer to scan through the string, and meanwhile update the HashMap.
 	 * If the character is already in the HashMap,
 	 * then move the left pointer to the right of the same character last found.
 	 */
 
-    public int solution1(String s) {
+    public int idea(String s) {
         if (s == null) return 0; // boundary
 
         // variables
@@ -32,6 +30,31 @@ public class LeetCode3 {
             }
             answer = Math.max(answer, right - left + 1);
             map.put(c, right); // always override previous
+        }
+
+        return answer;
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null) return 0; // boundary
+
+        // variables
+        int[] map = new int[128]; // default value: 0
+        int answer = 0;
+        int length = s.length();
+
+        if (length == 0) return 0; // boundary
+
+        for (int left = 0, right = 0; right < length; right++) {
+            char c = s.charAt(right);
+
+            if (map[c] > 0) {
+                left = Math.max(left, map[c]); // no need to move the pointer because it already point to the next subscript
+            }
+            answer = Math.max(answer, right - left + 1);
+            map[c] = right + 1;
+            // we use subscript + 1 because the int[128] always have default value 0
+            // in some boundary cases, this doesn't produce the right result
         }
 
         return answer;
