542f53b2ec51494ac5d4c267b9bb98b47bd45c72
yian <yianzhou2@outlook.com>
2018-08-24 11:40:28 +0800

    New solution 16

---
+++ src/algorithm/LeetCode16.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+// https://leetcode.com/problems/3sum-closest/description/
+
+import java.util.Arrays;
+
+public class LeetCode16 {
+    public int threeSumClosest(int[] nums, int target) {
+        int res = 0; // result value
+        if (nums == null || nums.length < 3) return res; // boundary
+
+        Arrays.sort(nums); // sort
+
+        int length = nums.length;
+        res = nums[0] + nums[1] + nums[length - 1]; // initial value
+        for (int i = 0; i < length - 2; i++) {
+            int lo = i + 1;
+            int hi = length - 1;
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
+
+                // move pointers
+                if (sum > target) hi--;
+                else lo++;
+
+                if (Math.abs(sum - target) < Math.abs(res - target)) res = sum;
+            }
+        }
+        return res;
+    }
+}
