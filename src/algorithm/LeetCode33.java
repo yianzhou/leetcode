214d952cb2aaa8ccdd4bdea2dab04cbc9a6b6ed1
yian <yianzhou2@outlook.com>
2018-08-29 16:19:03 +0800

    Update solution 33

--- src/algorithm/LeetCode33.java
+++ src/algorithm/LeetCode33.java
@@ -51,4 +51,36 @@ public class LeetCode33 {
         LeetCode33 solution = new LeetCode33();
         System.out.println(solution.numOfRotate(nums2));
     }
+
+    // solution 2
+
+    // 0, 1, 2, 3, 4
+    // 4, 0, 1, 2, 3
+    // 3, 4, 0, 1, 2
+    // 2, 3, 4, 0, 1
+    // 1, 2, 3, 4, 0
+
+    public int binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+
+        int lo = 0, hi = nums.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[lo] == target) return lo;
+            if (nums[hi] == target) return hi;
+
+            if (nums[lo] < nums[mid]) {
+                if (nums[mid] > target && target > nums[lo]) hi = mid - 1; // lo .. target .. mid .. hi
+                else lo = mid + 1;
+            }
+            else {
+                if (nums[mid] < target && target < nums[hi]) lo = mid + 1; // lo .. mid .. target .. hi
+                else hi = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
 }
